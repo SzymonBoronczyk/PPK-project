ac882a4a2f285ba6ea349cbaafe0348d93d3fcc8
SzymonBoronczyk <boronczyk.szymon@gmail.com>
2021-02-10 20:21:37 +0100

    Dodano funkcje do struktur plecaka i przedmiotow

--- plecak/projekt-plecak/main.cpp
+++ plecak/projekt-plecak/main.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
-
+#include"Item.h"
+#include"Bag.h"
 using namespace std;
 
 int main() {
-
-
+	Item item = createItem("Kielich", 1000, 14.20);
+	//show(item);
+	Item item1 = createItem("Pierscien", 2000, 15.50);
+	Bag bag = createBag(5000, 10);
+	add(bag, item);
+	add(bag, item1);
+	showBag(bag);
 
 	return 0;
 }
